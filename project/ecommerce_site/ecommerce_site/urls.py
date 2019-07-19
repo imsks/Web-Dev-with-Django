b3c5ddc942840971ef8dbe0fdcbc5679c09a96d4
sachin <sachinkshuklaoo7@gmail.com>
2019-07-19 12:46:32 +0530

    started ecommerce site project

--- project/ecommerce_site/ecommerce_site/urls.py
+++ project/ecommerce_site/ecommerce_site/urls.py
@@ -1,4 +1,4 @@
-"""textutils URL Configuration
+"""ecommerce_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('analyze', views.analyze, name="analyze"),
 ]
--- project/ecommerce_site/manage.py
+++ project/ecommerce_site/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textutils.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_site.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
